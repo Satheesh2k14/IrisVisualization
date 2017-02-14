46223aee0c3c07721d0bfd5b1f5a79c09313f22b
SATHEESH P <SATHEESH P>
2017-02-14 12:55:16 +0530

    K(1)nearest neighbor algorithm implemented

--- hello.py
+++ hello.py
@@ -9,6 +9,13 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
 
+#euclidean distance
+
+from scipy.spatial import distance
+
+def euc(a,b):
+    return distance.euclidean(a,b)
+
 #Our own classifier K means
 import random
 
@@ -20,10 +27,18 @@ class SatanKNN():
     def predict(self, X_test):
         predictions = []
         for row in X_test:
-            label = random.choice(self.y_train)
+            label = self.closest(row)
             predictions.append(label)
         return predictions
-
+    def closest(self, row):
+        best_dist = euc(row, self.X_train[0])
+        best_index = 0
+        for i in range(1, len(self.X_train)):
+            dist = euc(row, X_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+        return y_train[best_index]
 #from sklearn.neighbors import KNeighborsClassifier
 my_classifier = SatanKNN()
 
