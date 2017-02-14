9b2e7859d7365044466a15a0fc07461dd69765f6
SATHEESH P <SATHEESH P>
2017-02-14 12:36:17 +0530

    Self made random classifier to predict output

--- hello.py
+++ hello.py
@@ -9,26 +9,23 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
 
+#Our own classifier K means
+import random
 
-#decision tree
+class SatanKNN():
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+        
+    def predict(self, X_test):
+        predictions = []
+        for row in X_test:
+            label = random.choice(self.y_train)
+            predictions.append(label)
+        return predictions
 
-from sklearn import tree
-my_classifier = tree.DecisionTreeClassifier()
-
-my_classifier.fit(X_train, y_train)
-
-predictions = my_classifier.predict(X_test)
-print(predictions)
-
-#the original test predictions
-print(y_test)
-
-#Test accuracy
-from sklearn.metrics import accuracy_score
-print (accuracy_score(y_test, predictions))
-
-from sklearn.neighbors import KNeighborsClassifier
-my_classifier = KNeighborsClassifier()
+#from sklearn.neighbors import KNeighborsClassifier
+my_classifier = SatanKNN()
 
 my_classifier.fit(X_train, y_train)
 
