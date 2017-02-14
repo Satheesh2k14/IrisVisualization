f6b3e93dc9844e910b7af4365caaca48f180ed33
SATHEESH P <SATHEESH P>
2017-02-14 06:43:43 +0530

    Train Test Split Decision Tree Classifier accuracy measured

--- hello.py
+++ hello.py
@@ -1,26 +1,28 @@
 import numpy as np
-from sklearn import tree
 from sklearn.datasets import load_iris
+from sklearn.cross_validation import train_test_split
 
 iris = load_iris()
-test_idx = [0,50,100]
 
-#TRAINING DATA
-train_target = np.delete(iris.target, test_idx)
-train_data = np.delete(iris.data,test_idx,axis=0)
+X = iris.data
+y = iris.target
 
-#TEST DATA
-test_target = iris.target[test_idx]
-test_data = iris.data[test_idx]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
 
-clf = tree.DecisionTreeClassifier()
-clf.fit(train_data, train_target)
 
-print (clf.predict(test_data))
-print (test_target)
+#decision tree
 
-#visualisation
-from IPython.display import Image  
-dot_data = tree.export_graphviz(clf, out_file=None,feature_names=iris.feature_names,class_names=iris.target_names,filled=True, rounded=True,special_characters=True)  
-graph = pydotplus.graph_from_dot_data(dot_data)  
-Image(graph.create_png())  
+from sklearn import tree
+my_classifier = tree.DecisionTreeClassifier()
+
+my_classifier.fit(X_train, y_train)
+
+predictions = my_classifier.predict(X_test)
+print(predictions)
+
+#the original test predictions
+print(y_test)
+
+#Test accuracy
+from sklearn.metrics import accuracy_score
+print (accuracy_score(y_test, predictions))
